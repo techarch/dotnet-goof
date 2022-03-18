df7d0e7a51ea66ace0ad724b7a82fc25e240dc3a
Philippe Monnet <philippe.monnet@pulte.com>
2022-03-18 15:21:28 -0600

    Base version

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace dotNet_goof
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dotnet-goof");
+            Console.WriteLine("Enter the name of a file to display (e.g. README.md):");
+
+            string filePath = "README.md";
+            filePath = Console.ReadLine();
+            ShowFileContents(filePath);
+        }
+
+        private static void ShowFileContents(string filePath)
+        {
+            string fileContents = File.ReadAllText(filePath, Encoding.UTF8);
+            Console.WriteLine($"Contents of {filePath}:\n{fileContents}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotnet-goof
+
+This a very simple program to test Snyk code security analysis.
+
+Repository: https://github.com/techarch/dotnet-goof.git
